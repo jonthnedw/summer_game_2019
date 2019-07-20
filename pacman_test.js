a21606f5cc85768705048e3b14fcdbca8f969c38
davidko1221 <38569136+davidko1221@users.noreply.github.com>
2019-07-20 04:47:06 -0400

    Refactored code to use classes and other stuff
    1. Refactored the code to use classes. The old JS method of prototyping is really messy. However, while classes are cleaner, it's only supported in the latest versions of the major browsers. But that's okay. This is a learning exercise.
    
    2. Added an enemy circle that can be controlled by keyboard (w,a,s,d) and will eat any bouncing ball.

--- pacman_test.js
+++ pacman_test.js
@@ -13,97 +13,183 @@ function random(min,max) {
   return num;
 }
 
-// define Ball constructor
-
-function Ball(x, y, velX, velY, color, size) {
-  this.x = x;
-  this.y = y;
-  this.velX = velX;
-  this.velY = velY;
-  this.color = color;
-  this.size = size;
+class Shape {
+	constructor(x, y, velX, velY, exists) {
+		this.x = x;
+		this.y = y;
+		this.velX = velX;
+		this.velY = velY;
+		this.exists = exists;
+	}
+	
 }
 
-// define ball draw method
+class Ball extends Shape {
+	constructor(x, y, velX, velY, color, size, exists) {
+		super(x, y, velX, velY, exists);
+		
+		this.color = color;
+		this.size = size;
+	}
+	
+	
+	draw() {
+		ctx.beginPath();
+		ctx.fillStyle = this.color;
+		ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+		ctx.fill();
+	};
+	
+	update() {
+		if((this.x + this.size) >= width) {
+			this.velX = -(this.velX);
+		}
 
-Ball.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-  ctx.fill();
-};
+		if((this.x - this.size) <= 0) {
+			this.velX = -(this.velX);
+		}
 
-// define ball update method
+		if((this.y + this.size) >= height) {
+			this.velY = -(this.velY);
+		}
 
-Ball.prototype.update = function() {
-  if((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
-  }
+		if((this.y - this.size) <= 0) {
+			this.velY = -(this.velY);
+		}
 
-  if((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
-  }
+		this.x += this.velX;
+		this.y += this.velY;
+	};
+	
+	collisionDetect(balls) {
+		for(var j = 0; j < balls.length; j++) {
+			if(!(this === balls[j])) {
+				var dx = this.x - balls[j].x;
+				var dy = this.y - balls[j].y;
+				var distance = Math.sqrt(dx * dx + dy * dy);
 
-  if((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
-  }
+				if (distance < this.size + balls[j].size) {
+					balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
+				}
+			}
+		}
+	};
+}
 
-  if((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
-  }
+class EnemyCircle extends Shape {
+	constructor(x, y) {
+		super(x, y, 20, 20, true);
+		
+		this.color = 'white';
+		this.size = 10;
+	}
+	
+	
+	draw() {
+		ctx.beginPath();
+		ctx.lineWidth = 3;
+		ctx.strokeStyle = this.color;
+		ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+		ctx.stroke();
+	};
+	
+	update() {
+		if((this.x + this.size) >= width) {
+			this.x = width - this.size;
+		}
 
-  this.x += this.velX;
-  this.y += this.velY;
-};
+		if((this.x - this.size) <= 0) {
+			this.x = this.size;
+		}
 
-// define ball collision detection
+		if((this.y + this.size) >= height) {
+			this.y = height - this.size;
+		}
 
-Ball.prototype.collisionDetect = function() {
-  for(var j = 0; j < balls.length; j++) {
-    if(!(this === balls[j])) {
-      var dx = this.x - balls[j].x;
-      var dy = this.y - balls[j].y;
-      var distance = Math.sqrt(dx * dx + dy * dy);
+		if((this.y - this.size) <= 0) {
+			this.y = this.size;
+		}
 
-      if (distance < this.size + balls[j].size) {
-        balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
-      }
-    }
-  }
-};
+	};
+	
+	setControls() {
+		var _this = this;
+		window.onkeydown = function(e) {
+			if (e.keyCode === 65) {
+				_this.x -= _this.velX;
+			} else if (e.keyCode === 68) {
+				_this.x += _this.velX;
+			} else if (e.keyCode === 87) {
+				_this.y -= _this.velY;
+			} else if (e.keyCode === 83) {
+				_this.y += _this.velY;
+			}
+		}
+	}
+	
+	collisionDetect(balls) {
+		for(var j = 0; j < balls.length; j++) {
+			if(balls[j].exists) {
+				var dx = this.x - balls[j].x;
+				var dy = this.y - balls[j].y;
+				var distance = Math.sqrt(dx * dx + dy * dy);
+
+				if (distance < this.size + balls[j].size) {
+					balls[j].exists = false;
+				}
+			}
+		}
+	};
+	
+}
 
 // define array to store balls and populate it
 
 var balls = [];
 
 while(balls.length < 25) {
-  var size = random(10,20);
-  var ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the adge of the canvas, to avoid drawing errors
-    random(0 + size,width - size),
-    random(0 + size,height - size),
-    random(-7,7),
-    random(-7,7),
-    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
-    size
-  );
-  balls.push(ball);
+	var size = random(10,20);
+	
+	var ball = new Ball(
+		// ball position always drawn at least one ball width
+		// away from the adge of the canvas, to avoid drawing errors
+		random(0 + size,width - size),
+		random(0 + size,height - size),
+		random(-7,7),
+		random(-7,7),
+		'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+		size,
+		true
+	);
+	balls.push(ball);
 }
+
+var enemy = new EnemyCircle(
+		random(0 + size,width - size),
+		random(0 + size,height - size)
+	);
+
+enemy.setControls();
 
 // define loop that keeps drawing the scene constantly
 
 function loop() {
-  ctx.fillStyle = 'rgba(0,0,0,0.25)';
-  ctx.fillRect(0,0,width,height);
+	ctx.fillStyle = 'rgba(0,0,0,0.25)';
+	ctx.fillRect(0,0,width,height);
 
-  for(var i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
-  }
+	for(var i = 0; i < balls.length; i++) {
+		if (balls[i].exists) {
+			balls[i].draw();
+			balls[i].update();
+			balls[i].collisionDetect(balls);	
+		}
+	}
+	
+	enemy.draw();
+	enemy.update();
+	enemy.collisionDetect(balls);
 
-  requestAnimationFrame(loop);
+	requestAnimationFrame(loop);
 }
 
 
