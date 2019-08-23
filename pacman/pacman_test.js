5adca83d728c4c05d6576177bd6954cecdb69bab
davidko1221 <38569136+davidko1221@users.noreply.github.com>
2019-08-23 19:13:09 -0400

    styling changes for pacman

--- pacman/pacman_test.js
+++ pacman/pacman_test.js
@@ -3,8 +3,8 @@
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 
-var width = canvas.width = window.innerWidth;
-var height = canvas.height = window.innerHeight;
+var width = canvas.width = 700;
+var height = canvas.height = 700;
 
 var pScores = document.getElementById('scores');
 pScores.innerHTML = 'test';
