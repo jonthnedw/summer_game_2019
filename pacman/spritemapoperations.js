d65dc6431c6f651619ed05451136558a0e2eadfe
davidko1221 <38569136+davidko1221@users.noreply.github.com>
2019-08-23 21:24:17 -0400

    Some minor changes to main.html text and pacman.

---
+++ pacman/spritemapoperations.js
@@ -0,0 +1,10 @@
+
+/* Game board will use a 31x28 tile set, rendered from the sprite map (spritemap-384.png). 
+The canvas size will exactly match the size of the laid out tile set. 
+Each tile will be 24x24 pixels. 
+This means the game board’s pixel dimensions are: 672x744. */
+
+/* This document will map a character from the sprite map to a tile index. */
+
+
+
