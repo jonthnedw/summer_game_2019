da91b569d8e638c75089df372cc190f796972650
davidko1221 <38569136+davidko1221@users.noreply.github.com>
2019-08-26 18:48:01 -0400

    Pacman dev. Experimenting with the ts compiler

--- pacman/dist/SpriteOperations.js
+++ pacman/dist/SpriteOperations.js
@@ -1,19 +1,14 @@
-import * as fs from 'fs'; // This gets transpiled into: var fs = require('fs');
-
-
-
-/* Game board will use a 28x31 tile set, rendered from the sprite map (spritemap-384.png). 
-The canvas size will exactly match the size of the laid out tile set. 
-Each tile will be 24x24 pixels. 
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs"); // This gets transpiled into: var fs = require('fs');
+/* Game board will use a 28x31 tile set, rendered from the sprite map (spritemap-384.png).
+The canvas size will exactly match the size of the laid out tile set.
+Each tile will be 24x24 pixels.
 This means the game board’s pixel dimensions are: 672x744. */
-
 /* This document will map a character from the sprite map to a tile index. */
-
-
 // On success, returns the file data
 // On failure, returns the ErrorNoException
 // function ReadFileAsync(filename: string): Promise<any> {
-    
 //     return new Promise<any>((resolve,reject) => {
 //         fs.readFile(filename, (err, data:Buffer) => {
 //             if (err) 
@@ -23,89 +18,69 @@ This means the game board’s pixel dimensions are: 672x744. */
 //         });
 //     });
 // }
-
 class Tile {
-    srcTileX:number;
-    srcTtileY:number;
-    drawTileX:number;
-    drawTileY:number;
-
-    constructor(srcX:number, srcY:number, drawX:number, drawY:number) {
+    constructor(srcX, srcY, drawX, drawY) {
         this.srcTileX = srcX;
         this.srcTtileY = srcY;
         this.drawTileX = drawX;
         this.drawTileY = drawY;
     }
 }
-
-var m_gameBoardLoaded:boolean = false;
-var m_gameBoardTiles:Tile[] = null;
-var m_gameBoardWidth:number = -1;
-var m_gameBoardHeight:number = -1;
-
-function ProcessGameboardText(gameBoardStr:string) : boolean
-{
-    var charCount:number = 0;
- 
+var m_gameBoardLoaded = false;
+var m_gameBoardTiles = null;
+var m_gameBoardWidth = -1;
+var m_gameBoardHeight = -1;
+function ProcessGameboardText(gameBoardStr) {
+    var charCount = 0;
     var gameBoardRows = gameBoardStr.split('\n');
-    
     // The first row will determine the map width
     m_gameBoardWidth = gameBoardRows[0].length;
     m_gameBoardHeight = gameBoardRows.length;
-    
     // Check to make sure we have the correct number of map tiles
     // Make sure there isn't a newline after the last line. That will trip this error.
-    for(var i=0;i<gameBoardRows.length;i++)
-    {
-        if (gameBoardRows[i].length !== m_gameBoardWidth)
-        {
+    for (var i = 0; i < gameBoardRows.length; i++) {
+        if (gameBoardRows[i].length !== m_gameBoardWidth) {
             console.log("ERROR: Line " + i + " does not have the correct number of tiles! Map is malformed!");
             return false;
         }
     }
-
     // Initialize the tiles
-    m_gameBoardTiles = new Array<Tile>();
-
-    for(i=0;i<gameBoardRows.length;i++)
-    {
-        var row:string = gameBoardRows[i];
-
-        for(var j=0;j<row.length;j++)
-        {
-            switch(row[j])
-            {
-                case '┏':{
-                    m_gameBoardTiles.push(new Tile(i,j,0,0));
+    m_gameBoardTiles = new Array();
+    for (i = 0; i < gameBoardRows.length; i++) {
+        var row = gameBoardRows[i];
+        for (var j = 0; j < row.length; j++) {
+            switch (row[j]) {
+                case '┏': {
+                    m_gameBoardTiles.push(new Tile(i, j, 0, 0));
                     break;
                 }
-                case '━':{
-                    m_gameBoardTiles.push(new Tile(i,j,0,0));
+                case '━': {
+                    m_gameBoardTiles.push(new Tile(i, j, 0, 0));
                     break;
                 }
-                case '┓':{
-                    m_gameBoardTiles.push(new Tile(i,j,0,0));
+                case '┓': {
+                    m_gameBoardTiles.push(new Tile(i, j, 0, 0));
                     break;
                 }
-                case '┃':{
-                    m_gameBoardTiles.push(new Tile(i,j,0,0));
+                case '┃': {
+                    m_gameBoardTiles.push(new Tile(i, j, 0, 0));
                     break;
                 }
-                case '┗':{
-                    m_gameBoardTiles.push(new Tile(i,j,0,0));
+                case '┗': {
+                    m_gameBoardTiles.push(new Tile(i, j, 0, 0));
                     break;
                 }
-                case '┛':{
-                    m_gameBoardTiles.push(new Tile(i,j,0,0));
+                case '┛': {
+                    m_gameBoardTiles.push(new Tile(i, j, 0, 0));
                     break;
                 }
-                case '.':{
+                case '.': {
                     break;
                 }
-                case 'O':{
+                case 'O': {
                     break;
                 }
-                case ' ':{
+                case ' ': {
                     break;
                 }
                 default: {
@@ -115,81 +90,50 @@ function ProcessGameboardText(gameBoardStr:string) : boolean
                 }
             }
         }
-       
     }
-
     console.log("Processed Gameboard. Width=" + m_gameBoardWidth + ", Height=" + m_gameBoardHeight);
     m_gameBoardLoaded = true;
-
     return true;
 }
-
 // Eventually, this will return a promise so that all assets are downloaded asynchronously
-function DownloadAllAssets() : void {
-
+function DownloadAllAssets() {
     m_gameBoardLoaded = false;
-
-    fs.readFile("../GameBoard1.txt", (err, data:Buffer) => {
+    fs.readFile("../GameBoard1.txt", (err, data) => {
         if (err) {
             console.log("Error reading file");
         }
         else {
             console.log("Downloaded GameBoard1.txt");
-            var gameBoardStr:string = data.toString();
+            var gameBoardStr = data.toString();
             ProcessGameboardText(gameBoardStr);
         }
     });
-
     // ReadFileAsync("../GameBoard1.txt").then((result:Buffer) => {
     //     console.log("Downloaded GameBoard1.txt");
     //     var gameBoardStr:string = result.toString();
     //     ProcessGameboardText(gameBoardStr);
-
     // }).catch((err:NodeJS.ErrnoException) => {
-
     //     // Need to fail everything
     // });
-
-
 }
-
-
-
-class SpriteMap
-{
-    image:HTMLImageElement;
-    tilePxSize:number;
-
-	constructor() {
-		this.image = new HTMLImageElement();
+exports.DownloadAllAssets = DownloadAllAssets;
+class SpriteMap {
+    constructor() {
+        this.image = new HTMLImageElement();
         this.image.src = "spritemap-384.png";
         this.tilePxSize = 24;
-	}
-
-    drawGameTile(code:string, context:CanvasRenderingContext2D, cenX:number, cenY:number, tileX:number, tileY:number):void
-    {
-        var tileW:number = this.tilePxSize;
-        var tileH:number = this.tilePxSize;
-
-        context.drawImage(this.image, 
-            tileX * this.tilePxSize, tileY * this.tilePxSize, tileW, tileH,
-             cenX - tileW / 2, cenY - tileH / 2, tileW, tileH);
     }
-
+    drawGameTile(code, context, cenX, cenY, tileX, tileY) {
+        var tileW = this.tilePxSize;
+        var tileH = this.tilePxSize;
+        context.drawImage(this.image, tileX * this.tilePxSize, tileY * this.tilePxSize, tileW, tileH, cenX - tileW / 2, cenY - tileH / 2, tileW, tileH);
+    }
 }
-
-
-function DrawGameBoard(context:CanvasRenderingContext2D) : boolean
-{
+function DrawGameBoard(context) {
     if (!m_gameBoardLoaded) {
         return false;
     }
-
-
-    
-
     return true;
 }
-
-
-export { DownloadAllAssets, DrawGameBoard };
+exports.DrawGameBoard = DrawGameBoard;
+//# sourceMappingURL=SpriteOperations.js.map
--- pacman/src/PacmanMain.ts
+++ pacman/src/PacmanMain.ts
@@ -1,3 +1,31 @@
+import * as SpriteOperations from "./SpriteOperations";
+
+
+alert("starting loop...");
+
+var canvas:HTMLCanvasElement = document.querySelector('canvas');
+var ctx:CanvasRenderingContext2D = canvas.getContext('2d');
+
+var width:number = canvas.width = 700;
+var height:number = canvas.height = 700;
 
 
 
+
+// define loop that keeps drawing the scene constantly
+function loop(timestamp:number):void 
+{
+
+    //SpriteOperations.DrawGameBoard(ctx);
+
+	// This method will aim for 60 fps depending on your monitor refresh rate,
+	// and is always called before the next buffer drawn in the browser.
+	// This is a special method optimized for animations.
+	// The method will not run if the canvas is offscreen (I'll need to verify).
+	// Can be cancelled using cancelAnimationFrame()
+	requestAnimationFrame(loop);
+}
+
+
+
+loop(0);
