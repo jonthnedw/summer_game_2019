d69c1c367795b6055a93fc1b5158c9c87ff2e60e
jonthnedw <jon.ed.713@gmail.com>
2021-06-06 15:27:41 -0400

    Resetting with Express Server

--- README.md
+++
@@ -1,3 +0,0 @@
-# Super 8 gaming site
-
-Creating a platform where we can build games while learning game and web development.
--- asteroid/utils/Math2.ts
+++
@@ -1,8 +0,0 @@
-class Math2 {
-    // Does not return 0
-    static random(min: number = Number.MIN_VALUE, max:number = Number.MAX_VALUE): number {
-        return Math.floor(Math.random()*(max-min)) + min;;
-    };
-}
-
-export {Math2}
